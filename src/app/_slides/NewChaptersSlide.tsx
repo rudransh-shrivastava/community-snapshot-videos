0f1375f7f39dda6f3d78a83dc30ed47e3ccc0c49
Gopal <gopal.lohar.dev@gmail.com>
2025-11-15 17:37:07 +0530

    render fetched new chapters instead of dummy

--- src/app/_slides/NewChaptersSlide.tsx
+++ src/app/_slides/NewChaptersSlide.tsx
@@ -3,18 +3,22 @@ import { MapPin } from 'lucide-react'
 import Image from 'next/image'
 import WorldMap from '../../../public/worldmap.png'
 
-interface NewChapter {
-  chapter: string
-  lat: number
-  lon: number
+interface Chapter {
+  created_at: string
+  key: string
+  name: string
+  updated_at: string
+  longitude: number
+  latitude: number
+  country?: string
+  leaders?: string[]
+  region?: string
 }
 
 export function NewChaptersSlide({ data }: SlideComponentProps) {
-  const newChapters: NewChapter[] = data?.newChapters || [
-    { chapter: 'Somewhere, India', lat: 28.6139, lon: 77.209 },
-    { chapter: 'Somwhere', lat: 30.245896, lon: 19.243239 },
-    { chapter: 'Somewhere only we know', lat: 8.095344, lon: 77.487403 },
-  ]
+  const newChapters: Chapter[] = data || []
+
+  console.log('new cha', newChapters)
 
   const convertCoordsToStyle = (lat: number, lon: number) => {
     // the map is incomplete, it does not cover any area above greenland and anything below
@@ -26,7 +30,6 @@ export function NewChaptersSlide({ data }: SlideComponentProps) {
     const minLatRad = (minLat * Math.PI) / 180
     const latRad = (lat * Math.PI) / 180
 
-    // Mercator projection y values
     const mercMax = Math.log(Math.tan(Math.PI / 4 + maxLatRad / 2))
     const mercMin = Math.log(Math.tan(Math.PI / 4 + minLatRad / 2))
     const mercN = Math.log(Math.tan(Math.PI / 4 + latRad / 2))
@@ -49,12 +52,10 @@ export function NewChaptersSlide({ data }: SlideComponentProps) {
           <div
             key={index}
             className="absolute -translate-x-1/2 -translate-y-full opacity-90"
-            style={convertCoordsToStyle(chapter.lat, chapter.lon)}
+            style={convertCoordsToStyle(chapter.latitude, chapter.longitude)}
           >
-            <MapPin strokeWidth={1.5} size={48} />
-            <span className="absolute top-full left-1/2 -translate-x-1/2 font-bold">
-              {chapter.chapter}
-            </span>
+            <MapPin strokeWidth={1.5} size={32} />
+            <span className="absolute top-full left-1/2 -translate-x-1/2 font-bold"></span>
           </div>
         ))}
         <Image src={WorldMap} alt="Map of the world" />
