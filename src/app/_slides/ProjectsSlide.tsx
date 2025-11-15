357506603449ea6e564630e9e521cfbb9c9372d3
Gopal <gopal.lohar.dev@gmail.com>
2025-11-15 18:36:27 +0530

    render fetched projects instead of dummy

--- src/app/_slides/ProjectsSlide.tsx
+++ src/app/_slides/ProjectsSlide.tsx
@@ -1,32 +1,17 @@
 import { SlideComponentProps } from '@/types/slide'
-import { Folder, GitForkIcon, InfoIcon, Star, Users } from 'lucide-react'
+
+type Project = {
+  created_at: string
+  key: string
+  level: string
+  name: string
+  updated_at: string
+  description: string
+  leaders: string[]
+}
 
 export function ProjectsSlide({ slide, data }: SlideComponentProps) {
-  // const paginatedProjects = data as PaginatedResponse<Project> | undefined
-  // const projects = Array.isArray(data) ? (data as Project[]) : []
-
-  console.log(data)
-
-  const projects = [
-    {
-      created_at: '2025-02-07T17:49:37Z',
-      key: 'top-10-for-business-logic-abuse',
-      level: 'incubator',
-      name: 'OWASP Top 10 for Business Logic Abuse',
-      leaders: ['Name LastName'],
-      description: 'asdf',
-      updated_at: '2025-06-04T05:04:32Z',
-    },
-  ]
-
-  const temp = {
-    stars: 4430000,
-    forks: 12,
-    issues: 13,
-    repositories: 2,
-    contributors: 92,
-    maintainers: ['Me', 'Him'],
-  }
+  const projects = (data || []) as Project[]
 
   return (
     <div className="flex h-full flex-col items-center p-12 text-center">
@@ -39,29 +24,7 @@ export function ProjectsSlide({ slide, data }: SlideComponentProps) {
               className="flex flex-col gap-2 rounded-lg border-2 border-black/5 p-4 text-2xl"
             >
               <span className="font-bold">{project.name}</span>
-              <div>{temp.maintainers.join(', ')}</div>
-              <div className="flex justify-center gap-4">
-                <div className="flex gap-1">
-                  <Star className="opacity-75" />
-                  <span>{temp.stars.toLocaleString()}</span>
-                </div>
-                <div className="flex gap-1">
-                  <GitForkIcon className="opacity-75" />
-                  <span>{temp.forks.toLocaleString()}</span>
-                </div>
-                <div className="flex gap-1">
-                  <Users className="opacity-75" />
-                  <span>{temp.contributors.toLocaleString()}</span>
-                </div>
-                <div className="flex gap-1">
-                  <InfoIcon className="opacity-75" />
-                  <span>{temp.issues.toLocaleString()}</span>
-                </div>
-                <div className="flex gap-1">
-                  <Folder className="opacity-75" />
-                  <span>{temp.repositories.toLocaleString()}</span>
-                </div>
-              </div>
+              <div>{project.leaders.join(', ')}</div>
             </div>
           ))
         ) : (
